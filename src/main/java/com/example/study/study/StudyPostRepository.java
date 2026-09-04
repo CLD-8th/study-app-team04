fa76553f8d2035928f56eee378b7a94dbc0b2e7b
JG-127 <wjdrlf0127@gmail.com>
2026-09-04 16:25:04 +0900

    feat: TODO 61 구현

--- src/main/java/com/example/study/study/StudyPostRepository.java
+++ src/main/java/com/example/study/study/StudyPostRepository.java
@@ -39,4 +39,6 @@ public interface StudyPostRepository extends JpaRepository<StudyPost, Long> {
      * 반환형태    List<StudyPost>
      * 동작결과    마이페이지에서 내 모집글이 최신순으로 나옴
      */
+    @EntityGraph(attributePaths = {"writer"})
+    List<StudyPost> findAllByWriterIdOrderByIdDesc(Long WriterId);
 }
